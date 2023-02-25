37312edaf30441f6ef4b7b9aaa6f04bd72ce0511
riomljr <22552727+riomljr@users.noreply.github.com>
2023-02-25 10:13:21 -0600

    add post views

--- db/migrate/20230225141124_create_posts.rb
+++ db/migrate/20230225141124_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.text :body
-      t.references :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
